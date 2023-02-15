c623e15322d3a88a4c276b00630cc6310e963883
amanj21 <ajohal21@gmail.com>
2023-02-14 21:42:13 -0800

    Added complete comments for all methods

--- src/main/model/Report.java
+++ src/main/model/Report.java
@@ -19,35 +19,58 @@ public class Report {
         vaccineInfo = new ArrayList<>();
     }
 
-
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS: Gets personName
     public String getPersonName() {
         return personName;
     }
 
+    // REQUIRES:
+    // MODIFIES: this
+    // EFFECTS: sets personName
     public void setPersonName(String personName) {
         this.personName = personName;
     }
 
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS: Gets age
     public int getAge() {
         return age;
     }
 
+    // REQUIRES:
+    // MODIFIES: this
+    // EFFECTS: sets age
     public void setAge(int age) {
         this.age = age;
     }
 
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS: gets country name
     public String getCountryName() {
         return countryName;
     }
 
+    // REQUIRES:
+    // MODIFIES: this
+    // EFFECTS: sets country name
     public void setCountryName(String countryName) {
         this.countryName = countryName;
     }
 
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS: gets vaccine titles
     public ArrayList<String> getVaccineInfo() {
         return vaccineInfo;
     }
 
+    // REQUIRES:
+    // MODIFIES: this
+    // EFFECTS: adds titles to vaccine arrayList
     public void addVaccineInfo(String vaccine) {
         vaccineInfo.add(vaccine);
     }
